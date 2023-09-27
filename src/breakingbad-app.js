260ce8052a983e2007b67a698e7ca96e715e706c
Donato Huayhua <huayhuadonatelo@gmail.com>
2023-09-27 00:15:55 -0400

    update quote

--- src/breakingbad-app.js
+++ src/breakingbad-app.js
@@ -1,10 +1,25 @@
 
 /**
  * 
+ * @returns {Promise<Object>} quote information
+ */
+const fetchQuote = async () => {
+    const res = await fetch('https://rickandmortyapi.com/api/character/1,183');
+    const data = await res.json();
+
+    console.log(data)
+    return data;
+}
+
+
+/**
+ * 
  * @param {HTMLDivElement} element 
  */
-export const BreakingbadApp = (element) => {
+export const BreakingbadApp = async (element) => {
 
     document.querySelector('#app-title').innerHTML = 'Breakingbad App';
     element.innerHTML = 'Loading...';
+    const quote = await fetchQuote();
+    element.innerHTML = 'Tenemos data!';
 }
